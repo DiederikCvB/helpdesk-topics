b21609e4009c6a9603e4793de1f7b8ab7bef1354
DiederikCvB <diederik.cvb@gmail.com>
2023-04-21 13:34:53 +0200

    Fix variable naming for in production settings

--- Development/ciphix_django/topic_classification/views.py
+++ Development/ciphix_django/topic_classification/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
+from django.conf import settings
 
+import os
 import joblib
 import pandas as pd
 
+
 # Load models and preprocessing once
 class TopicPredictor:
-    def __init__(self, version):
-        self.clean_df = joblib.load((os.path.join(settings.STATIC_ROOT,f'clean_df{version}.joblib'))
-        self.preprocess = joblib.load((os.path.join(settings.STATIC_ROOT,f'preprocess{version}.joblib'))
+    def __init__(self, settings, version=''):
+        self.clean_df = joblib.load(os.path.join(settings.STATIC_ROOT,f'clean_df{version}.joblib'))
+        self.preprocess = joblib.load(os.path.join(settings.STATIC_ROOT,f'preprocess{version}.joblib'))
 
-        self.nmf = joblib.load((os.path.join(settings.STATIC_ROOT,f'nmf{version}.joblib'))
-        self.vectorizer = joblib.load((os.path.join(settings.STATIC_ROOT,f'vectorizer{version}.joblib'))
+        self.nmf = joblib.load(os.path.join(settings.STATIC_ROOT,f'nmf{version}.joblib'))
+        self.vectorizer = joblib.load(os.path.join(settings.STATIC_ROOT,f'vectorizer{version}.joblib'))
         self.topic_descr = self.get_topics_descr()
 
 
@@ -26,23 +29,23 @@ class TopicPredictor:
             res.append(descr)
         return res
 
-    def predict(raw_text):
+    def predict(self, raw_text):
         """
-        Predicts the topic of a new piece of raw text. 
+        Predicts the topic of a new piece of raw text 
         Returns: topic number and description of that topic in word scores
         """
-        inf_doc = pd.DataFrame([raw_text], dtype=str, columns=['text'])
+        doc = pd.DataFrame([raw_text], dtype=str, columns=['text'])
 
-        inf_doc['clean_text'] = inf_clean_df(inf_doc['text'])
-        inf_doc['processed_text'] = inf_preprocess(inf_doc['clean_text'])
+        doc['clean_text'] = self.clean_df(doc['text'])
+        doc['processed_text'] = self.preprocess(doc['clean_text'])
 
-        newdata_vectorized = vectorizer.transform(inf_doc["processed_text"])
-        newdata_nmf = nmf.transform(newdata_vectorized) 
-        print("Processed text:", inf_doc['processed_text'])
+        vectorized = self.vectorizer.transform(doc["processed_text"])
+        nmf = self.nmf.transform(vectorized) 
+        print("Processed text:", doc['processed_text'])
 
-        predicted_topic = [each.argsort()[::-1][0] for each in newdata_nmf]
+        predicted_topic = [each.argsort()[::-1][0] for each in nmf]
         topic_descr = self.topic_descr[predicted_topic[0]]
         return predicted_topic, topic_descr
 
-TopicPredictor(version = '_customers')
+topic_predictor = TopicPredictor(settings, '_customers')
 # Create your views here.
