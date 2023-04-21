3f097a90a0a82f5eddc180cc4378e6088915bcb5
DiederikCvB <diederik.cvb@gmail.com>
2023-04-21 16:58:37 +0200

    minimal prediction reporting on index page

--- Development/ciphix_django/ciphix_topics/views.py
+++ Development/ciphix_django/ciphix_topics/views.py
@@ -21,14 +21,13 @@ topic_model = TopicPredictor(nmf, vectorizer, version)
 # Create your views here.
 
 def index(request):
-    image_uri = None
     predicted_label = None
 
     if request.method == 'POST':
-        # in case of POST: get the uploaded image from the form and process it
+        # in case of POST: get the uploaded text from the form and process it
         form = CommentForm(request.POST)
         if form.is_valid():
-            # retrieve the uploaded image and convert it to bytes (for PyTorch)
+            # retrieve the uploaded text
             text = form.cleaned_data['comment']
             print('%'*50)
 
@@ -36,21 +35,23 @@ def index(request):
             df = preproc.clean(text)
             df = preproc.preprocess(df)
             print('Preprocessed: \n',df, '\n')
-            
-            # get predicted label with previously implemented PyTorch function
+
+            # get predicted label
             try:
                 predicted_label, descr = topic_model.predict(df)
+                print(f"PREDICT: {predicted_label}")
             except RuntimeError as re:
                 print(re)
 
     else:
-        # in case of GET: simply show the empty form for uploading images
+        # in case of GET: simply show the empty form for uploading text
         form = CommentForm()
 
     # pass the form, image URI, and predicted label to the template to be rendered
     context = {
         'form': form,
         'predicted_label': predicted_label,
+        'entered_text' : text,
         'descr' : descr,
     }
     return render(request, 'ciphix_topics/index.html', context)
